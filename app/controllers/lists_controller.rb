4053bb6ae0e97279c8cb2cd48eef2e40ab7cc2fa
Simon Lance <simonlance200@hotmail.com>
2023-01-28 16:25:48 +0000

    new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,7 +12,7 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
+    @list = List.new(lists_params)
     if @list.save
       redirect_to list_path(@list)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # get '/lists', to: 'lists#index'
-  # get '/lists/:id', to: 'lists#show'
-  # get '/lists/new', to: 'lists#new'
-  get "lists", to: "lists#index"
-  get "lists/:id", to: "lists#show", as: :list
-  get "lists/new", to: "lists#new",  as: :new_list
-  post "lists", to: "lists#create"
-  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
-  post "lists/:id/bookmarks", to: "bookmarks#create"
-  delete "bookmarks/:id", to: "bookmarks#destroy"
+  root to: "lists#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:destroy]
+  end
 end
