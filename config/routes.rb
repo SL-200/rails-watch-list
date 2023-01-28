c3df8f1ba25052145b8a258ff14998ebc87babc5
Simon Lance <simonlance200@hotmail.com>
2023-01-28 15:40:42 +0000

    list index view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/lists', to: 'lists#index'
+  # get '/lists/:id', to: 'lists#show'
+  # get '/lists/new', to: 'lists#new'
+  get "lists", to: "lists#index"
+  get "lists/:id", to: "lists#show", as: :list
+  get "lists/new", to: "lists#new",  as: :new_list
+  post "lists", to: "lists#create"
+  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
+  post "lists/:id/bookmarks", to: "bookmarks#create"
+  delete "bookmarks/:id", to: "bookmarks#destroy"
 end
